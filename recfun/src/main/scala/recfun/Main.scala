13850c344c6d7af43c670978ab506943e7a09000
Erni <ernesto.reig@gmail.com>
2014-05-23 17:34:52 +0200

    1st and 2nd exercises

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,15 +15,24 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if(c == 0 || c == r) 1
-    if(c == 1 || c == (r - 1)) r
-    pascal(c - 1, r -1) + pascal(c, r -1)
+    if (c == 0 || c == r) 1
+    else if (c == 1 || c == (r - 1)) r
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceHelper(chars: List[Char], op: Int): Boolean = {
+      if(op < 0 || (chars.isEmpty && op > 0)) false
+      else if (chars.isEmpty && op == 0) true
+      else if (chars.head == '(') balanceHelper(chars.tail, op + 1)
+      else if (chars.head == ')') balanceHelper(chars.tail, op - 1)
+      else balanceHelper(chars.tail, op)
+    }
+    balanceHelper(chars, 0)
+  }
 
   /**
    * Exercise 3
